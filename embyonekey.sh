26d1b1007c28568ce4ffea2679ab16721e65b0ed
temp <temp@example.com>
2021-10-06 10:32:25 +0800

    init code

---
+++ embyonekey.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd /var/packages/EmbyServer/target/mono/bin
+curl https://raw.githubusercontent.com/s1oz/embyonekey/master/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
+./cert-sync /etc/ssl/certs/ca-certificates.crt
+cd /usr/local/etc/nginx/conf.d/*-*-*-*
+wget https://raw.githubusercontent.com/Ethan-7seng/embyonekey/master/user.conf
+nginx -s reload
+
