4f0a95ae07d42d125ee3e154e26e51cc51d29849
temp <temp@example.com>
2021-10-06 11:18:15 +0800

    add server config

--- embyonekey.sh
+++ embyonekey.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-cd /var/packages/EmbyServer/target/mono/bin
-curl https://raw.githubusercontent.com/s1oz/embyonekey/master/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
-./cert-sync /etc/ssl/certs/ca-certificates.crt
-cd /usr/local/etc/nginx/conf.d/*-*-*-*
-wget https://raw.githubusercontent.com/Ethan-7seng/embyonekey/master/user.conf
+curl https://raw.githubusercontent.com/virualv/embyonekey/server/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
+update-ca-certificates --fresh
+cd /usr/local/nginx/conf/vhost/
+wget --no-check-certificate https://raw.githubusercontent.com/virualv/embyonekey/server/proxy.conf -O embyproxy.conf
+mkdir -p /usr/local/nginx/conf/ssl/mb3admin.com/
+cd /usr/local/nginx/conf/ssl/mb3admin.com/
+wget --no-check-certificate https://raw.githubusercontent.com/virualv/embyonekey/server/mb3admin.com.cert.pem
+wget --no-check-certificate https://raw.githubusercontent.com/virualv/embyonekey/server/mb3admin.com.key.pem
 nginx -s reload
+
 
---
+++ embyonekey_acc.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+curl https://raw.fastgit.org/virualv/embyonekey/server/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
+update-ca-certificates --fresh
+cd /usr/local/nginx/conf/vhost/
+wget --no-check-certificate https://raw.fastgit.org/virualv/embyonekey/server/proxy.conf -O embyproxy.conf
+mkdir -p /usr/local/nginx/conf/ssl/mb3admin.com/
+cd /usr/local/nginx/conf/ssl/mb3admin.com/
+wget --no-check-certificate https://raw.fastgit.org/virualv/embyonekey/server/mb3admin.com.cert.pem
+wget --no-check-certificate https://raw.fastgit.org/virualv/embyonekey/server/mb3admin.com.key.pem
+nginx -s reload
+
+
